e282ae3cfb55998c0bc77148c7679ca0faef152d
xXJanisXx <Janis.Minecrafto@gmx.de>
2026-02-15 12:09:05 +0100

    chore: remove .idea folder

--- prefixes-paper/src/main/kotlin/app/simplecloud/plugin/prefixes/paper/listener/LuckPermsListener.kt
+++ prefixes-paper/src/main/kotlin/app/simplecloud/plugin/prefixes/paper/listener/LuckPermsListener.kt
@@ -1,6 +1,5 @@
 package app.simplecloud.plugin.prefixes.paper.listener
 
-import app.simplecloud.plugin.prefixes.api.PrefixesApi
 import net.luckperms.api.LuckPerms
 import net.luckperms.api.event.user.UserDataRecalculateEvent
 import org.bukkit.plugin.Plugin
@@ -8,7 +7,6 @@ import org.bukkit.plugin.Plugin
 class LuckPermsListener(
     private val plugin: Plugin,
     private val luckPerms: LuckPerms,
-    private val api: PrefixesApi
 ) {
 
     fun init() {
@@ -21,7 +19,5 @@ class LuckPermsListener(
     private fun onUserUpdate(event: UserDataRecalculateEvent) {
         if(groups.getOrDefault(event.user.uniqueId.toString(), "") == event.user.primaryGroup) return
         groups[event.user.uniqueId.toString()] = event.user.primaryGroup
-        api.setWholeName(event.user.uniqueId, event.user.primaryGroup)
     }
-
 }
